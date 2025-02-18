4056306bbca0da648463d2144fca3670ec86f410
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-18 12:57:26 +0900

    feat : DomainException과 전역 예외 처리 핸들러를 설정한다.

---
+++ src/main/java/com/seoulmilk/core/exception/DomainException.java
@@ -0,0 +1,28 @@
+package com.seoulmilk.core.exception;
+
+import com.seoulmilk.core.exception.error.BaseErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class DomainException extends RuntimeException {
+
+  private HttpStatus httpStatus;
+
+  private String code;
+
+  public DomainException(String message) {
+    super(message);
+  }
+
+  public DomainException(HttpStatus httpStatus, String message) {
+    super(message);
+    this.httpStatus = httpStatus;
+  }
+
+  public DomainException(HttpStatus httpStatus, BaseErrorCode<?> errorCode) {
+    super(errorCode.getMessage());
+    this.httpStatus = httpStatus;
+    this.code = errorCode.name();
+  }
+}
---
+++ src/main/java/com/seoulmilk/core/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.seoulmilk.core.exception;
+
+import com.seoulmilk.core.presentation.ErrorResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Optional;
+
+@RestControllerAdvice
+@Log4j2
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  public final ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
+    log.error("ERROR ::: [AllException] ", ex);
+    ErrorResponse errorResponse = ErrorResponse.createErrorResponse().statusCode(500).exception(ex).build();
+    return ResponseEntity.internalServerError().body(errorResponse);
+  }
+
+  @ExceptionHandler(DomainException.class)
+  public final ResponseEntity<ErrorResponse> handleDomainException(DomainException ex) {
+    HttpStatus httpStatus = Optional.ofNullable(ex.getHttpStatus()).orElse(HttpStatus.INTERNAL_SERVER_ERROR);
+    log.error("ERROR ::: [DomainException] ", ex);
+    ErrorResponse errorResponse = ErrorResponse.createDomainErrorResponse().statusCode(httpStatus.value()).exception(ex).build();
+    return ResponseEntity.status(httpStatus).body(errorResponse);
+  }
+}
