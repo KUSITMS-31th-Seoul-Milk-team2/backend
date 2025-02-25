4cf665dcec2fe73bd42d375b6529a2883283fa97
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-25 17:46:41 +0900

    chore: OpenFeign 의존성을 추가한다.

--- src/main/java/com/seoulmilk/BackendApplication.java
+++ src/main/java/com/seoulmilk/BackendApplication.java
@@ -3,11 +3,13 @@ package com.seoulmilk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
+@EnableFeignClients
 public class BackendApplication {
 
 	public static void main(String[] args) {
