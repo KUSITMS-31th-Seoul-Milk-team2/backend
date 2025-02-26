9d06dd4cb6a03304bc58ee201766bbada904ec89
chyun7114 <chyun7114@naver.com>
2025-02-26 23:34:19 +0900

    feat: 세금계산서 검증 로직 작성 완료

--- src/main/java/com/seoulmilk/receipt/presentation/dto/response/TaxReceiptValidationResponse.java
+++ src/main/java/com/seoulmilk/receipt/presentation/dto/response/TaxReceiptValidationResponse.java
@@ -1,11 +1,14 @@
 package com.seoulmilk.receipt.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record TaxReceiptValidationResponse(
+        @JsonProperty
         @Schema(description = "진위확인", example = "(\"0\": false, \"1\": true)")
         String resAuthenticity,
 
+        @JsonProperty
         @Schema(
                 description = "진위확인 내용",
                 example = "\t(ex. \"조회 요청한 전자세금계산서는 2022년 01월 10일 발급된 사실이 있습니다.”)"
