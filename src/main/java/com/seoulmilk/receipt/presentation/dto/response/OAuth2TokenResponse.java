f52a92dddd0aaa463e5e4e6e3de7f4ea73cd4133
chyun7114 <chyun7114@naver.com>
2025-02-26 22:48:36 +0900

    feat: accessToken 관련 deserialization 오류 해결

--- src/main/java/com/seoulmilk/receipt/presentation/dto/response/OAuth2TokenResponse.java
+++ src/main/java/com/seoulmilk/receipt/presentation/dto/response/OAuth2TokenResponse.java
@@ -1,12 +1,16 @@
 package com.seoulmilk.receipt.presentation.dto.response;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record OAuth2TokenResponse(
-        @Schema(description = "accessToken", example = "OPENJ API 서버 접근용 accessToken")
-        String accessToken,
-
-        @Schema(description = "RefreshToken", example = "OPEN API 서버 전용 refreshToken")
-        String refreshToken
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("token_type") String tokenType,
+        @JsonProperty("expires_in") Long expiresIn,
+        @JsonProperty("scope") String scope
 ) {
+    @JsonCreator
+    public OAuth2TokenResponse {
+        // 아무것도 안 해도 됨. 단순히 Jackson이 인식하게 하는 역할
+    }
 }
