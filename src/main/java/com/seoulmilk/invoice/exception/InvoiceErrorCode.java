c2782c9b5ecfb45532027dc7759d44842767e9eb
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-27 01:05:25 +0900

    feat: 세금계산서 에러 코드를 추가한다.

--- src/main/java/com/seoulmilk/invoice/exception/InvoiceErrorCode.java
+++ src/main/java/com/seoulmilk/invoice/exception/InvoiceErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum InvoiceErrorCode implements BaseErrorCode<DomainException> {
-    FAILED_TO_CREATE_JSON(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 생성 중 에러가 발생했습니다.");
+    FAILED_TO_CREATE_JSON(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 생성 중 에러가 발생했습니다."),
+    EMPTY_FILE(HttpStatus.BAD_REQUEST, "파일이 비어있습니다."),
+    NOT_SUPPORTED_EXTENSION(HttpStatus.BAD_REQUEST, "지원하지 않는 확장자입니다. (JPEG, PNG, PDF만 허용)"),
+    NOT_SUPPORTED_FILE(HttpStatus.BAD_REQUEST, "지원하지 않는 파일 형식입니다. (JPEG, PNG, PDF만 허용)");
 
     private final HttpStatus httpStatus;
 
