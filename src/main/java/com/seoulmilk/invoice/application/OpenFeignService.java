a62a1c0fb614d52e517e509a7023197cab80e458
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-27 01:05:15 +0900

    feat: 응용계층을 비즈니스 로직에 집중하도록 변경한다.

--- src/main/java/com/seoulmilk/invoice/application/OpenFeignService.java
+++ src/main/java/com/seoulmilk/invoice/application/OpenFeignService.java
@@ -1,63 +1,44 @@
 package com.seoulmilk.invoice.application;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.seoulmilk.invoice.dto.request.OcrRequest;
+import com.seoulmilk.invoice.domain.service.OcrEngine;
+import com.seoulmilk.invoice.domain.value.FileMetaData;
 import com.seoulmilk.invoice.dto.response.OcrResponse;
 import com.seoulmilk.invoice.exception.InvoiceErrorCode;
-import com.seoulmilk.invoice.infrastructure.configuration.properties.OcrProperties;
+import com.seoulmilk.invoice.infrastructure.converter.OcrRequestConverter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class OpenFeignService {
-    private final OpenFeignClient openFeignClient;
-    private final OcrProperties ocrProperties;
-    private final ObjectMapper objectMapper;
+    private final OcrEngine ocrEngine;
+    private final OcrRequestConverter requestConverter;
+    private final OcrRequestFactory requestFactory;
 
     public OcrResponse processImg(MultipartFile file) {
-        String message = createMessage(file.getOriginalFilename());
-        return openFeignClient.extractText(message, file);
+        validateFilePresence(file);
+        FileMetaData fileMetaData = createFileMetaData(file);
+        String message = createRequestMessage(fileMetaData);
+        return ocrEngine.extractText(message, file);
     }
 
-    private String createMessage(String filename) {
-        try {
-            OcrRequest request = new OcrRequest(
-                    ocrProperties.getVersion(),
-                    ocrProperties.getRequestId(),
-                    System.currentTimeMillis(),
-                    ocrProperties.getLang(),
-                    List.of(new OcrRequest.ImageInfo(
-                            getFileExtension(filename),
-                            removeExtension(filename)
-                    ))
-            );
+    private FileMetaData createFileMetaData(MultipartFile file) {
+        return new FileMetaData(
+                file.getOriginalFilename(),
+                file.getContentType()
+        );
+    }
 
-            return objectMapper.writeValueAsString(request);
-        } catch (Exception e) {
-            throw InvoiceErrorCode.FAILED_TO_CREATE_JSON.toException();
+    private void validateFilePresence(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw InvoiceErrorCode.EMPTY_FILE.toException();
         }
     }
 
-
-    private String getFileExtension(String filename) {
-        int lastDotIndex = filename.lastIndexOf('.');
-        if (lastDotIndex > 0) {
-            return filename.substring(lastDotIndex + 1).toLowerCase();
-        }
-        return ocrProperties.getDefaultFormat();
-    }
-
-    private String removeExtension(String filename) {
-        int lastDotIndex = filename.lastIndexOf('.');
-        if (lastDotIndex > 0) {
-            return filename.substring(0, lastDotIndex);
-        }
-        return filename;
+    private String createRequestMessage(FileMetaData metaData) {
+        return requestConverter.toJson(requestFactory.create(metaData));
     }
 }
