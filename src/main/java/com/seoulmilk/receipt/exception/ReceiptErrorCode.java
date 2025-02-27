57ce66b0ae955b5c1d1fe46daee7f7c347f0c20b
chyun7114 <chyun7114@naver.com>
2025-02-27 11:17:27 +0900

    feat: ReceiptErrorCode 에러 코드 추가 및 일부 수정

--- src/main/java/com/seoulmilk/receipt/exception/ReceiptErrorCode.java
+++ src/main/java/com/seoulmilk/receipt/exception/ReceiptErrorCode.java
@@ -9,9 +9,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ReceiptErrorCode implements BaseErrorCode<DomainException> {
-    INVALID_FORMAT_ERROR(HttpStatus.NOT_FOUND, "텍스트의 형식이 일치하지 않습니다."),
-    UNAUHORIZED_USER_ERROR(HttpStatus.FORBIDDEN, "권한이 없는 유저가 사용중입니다."),
-    OAUTH2_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "oAuth2 토큰 에러입니다.");
+    INVALID_FORMAT_ERROR(HttpStatus.BAD_REQUEST, "텍스트의 형식이 일치하지 않습니다."),
+    UNAUTHORIZED_USER_ERROR(HttpStatus.FORBIDDEN, "권한이 없는 유저가 사용중입니다."),
+    OAUTH2_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "oAuth2 토큰 에러입니다."),
+    JSON_DESERIALIZED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,
+            "JSON 역직렬화 에러입니다. 응답 포맷이 맞지 않습니다.");
 
     private final HttpStatus httpStatus;
 
