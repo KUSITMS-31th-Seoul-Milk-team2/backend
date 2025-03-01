9eab96918df73c15e9de6e00648b87dc9e5b51cc
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-02 01:46:38 +0900

    feat: InvoiceErrorCode를 추가한다.

--- src/main/java/com/seoulmilk/invoice/exception/InvoiceErrorCode.java
+++ src/main/java/com/seoulmilk/invoice/exception/InvoiceErrorCode.java
@@ -12,7 +12,8 @@ public enum InvoiceErrorCode implements BaseErrorCode<DomainException> {
     FAILED_TO_CREATE_JSON(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 생성 중 에러가 발생했습니다."),
     EMPTY_FILE(HttpStatus.BAD_REQUEST, "파일이 비어있습니다."),
     NOT_SUPPORTED_EXTENSION(HttpStatus.BAD_REQUEST, "지원하지 않는 확장자입니다. (JPEG, PNG, PDF만 허용)"),
-    NOT_SUPPORTED_FILE(HttpStatus.BAD_REQUEST, "지원하지 않는 파일 형식입니다. (JPEG, PNG, PDF만 허용)");
+    NOT_SUPPORTED_FILE(HttpStatus.BAD_REQUEST, "지원하지 않는 파일 형식입니다. (JPEG, PNG, PDF만 허용)"),
+    INVALID_SUPPLIER_NUMBER_FORMAT(HttpStatus.BAD_REQUEST, "사업자등록번호 형식이 올바르지 않습니다.");
 
     private final HttpStatus httpStatus;
 
