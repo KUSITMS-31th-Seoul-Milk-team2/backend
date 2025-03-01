03ebcd52786eddb1050b9ee34706bcafcc5c940f
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-02 02:57:03 +0900

    feat: 예외 코드를 추가한다.

---
+++ src/main/java/com/seoulmilk/invoice/exception/EventErrorCode.java
@@ -0,0 +1,21 @@
+package com.seoulmilk.invoice.exception;
+
+import com.seoulmilk.core.exception.DomainException;
+import com.seoulmilk.core.exception.error.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum EventErrorCode implements BaseErrorCode<DomainException> {
+    FAILED_TO_PUBLISH_EVENT(HttpStatus.INTERNAL_SERVER_ERROR, "이벤트 발행 중 에러가 발생했습니다.");
+    private final HttpStatus httpStatus;
+
+    private final String message;
+
+    @Override
+    public DomainException toException() {
+        return new DomainException(httpStatus, this);
+    }
+}
