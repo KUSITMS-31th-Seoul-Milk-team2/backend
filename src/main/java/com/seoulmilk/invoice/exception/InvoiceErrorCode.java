f4c76bdf99a5b975553fe974509dae884dc35e03
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-02 02:57:03 +0900

    feat: 예외 코드를 추가한다.

--- src/main/java/com/seoulmilk/invoice/exception/InvoiceErrorCode.java
+++ src/main/java/com/seoulmilk/invoice/exception/InvoiceErrorCode.java
@@ -13,7 +13,9 @@ public enum InvoiceErrorCode implements BaseErrorCode<DomainException> {
     EMPTY_FILE(HttpStatus.BAD_REQUEST, "파일이 비어있습니다."),
     NOT_SUPPORTED_EXTENSION(HttpStatus.BAD_REQUEST, "지원하지 않는 확장자입니다. (JPEG, PNG, PDF만 허용)"),
     NOT_SUPPORTED_FILE(HttpStatus.BAD_REQUEST, "지원하지 않는 파일 형식입니다. (JPEG, PNG, PDF만 허용)"),
-    INVALID_SUPPLIER_NUMBER_FORMAT(HttpStatus.BAD_REQUEST, "사업자등록번호 형식이 올바르지 않습니다.");
+    INVALID_SUPPLIER_NUMBER_FORMAT(HttpStatus.BAD_REQUEST, "사업자등록번호 형식이 올바르지 않습니다."),
+    INVALID_OCR_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR, "OCR 결과가 올바르지 않습니다."),
+    MISSING_REQUIRED_FIELD(HttpStatus.BAD_REQUEST, "필수 필드가 누락되었습니다."),;
 
     private final HttpStatus httpStatus;
 
