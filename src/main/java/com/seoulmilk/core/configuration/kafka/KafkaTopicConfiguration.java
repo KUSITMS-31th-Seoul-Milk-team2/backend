70d3fd4f39df0e403b68d227bc042d36f34286de
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-02 01:48:08 +0900

    chore: KafkaTopicConfiguration을 설정한다.

---
+++ src/main/java/com/seoulmilk/core/configuration/kafka/KafkaTopicConfiguration.java
@@ -0,0 +1,22 @@
+package com.seoulmilk.core.configuration.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaTopicConfiguration {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public NewTopic ocrResultTopic() {
+        return TopicBuilder.name(kafkaProperties.getTopic())
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+}
