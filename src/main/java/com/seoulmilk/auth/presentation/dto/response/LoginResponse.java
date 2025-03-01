ee9db9917f4d65d1b9db9b700600c71c07b173a2
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-01 20:33:36 +0900

    feat: LoginResponse 응답 구조를 변경한다.

--- src/main/java/com/seoulmilk/auth/presentation/dto/response/LoginResponse.java
+++ src/main/java/com/seoulmilk/auth/presentation/dto/response/LoginResponse.java
@@ -1,29 +1,29 @@
 package com.seoulmilk.auth.presentation.dto.response;
 
 import com.seoulmilk.emp.domain.entity.Emp;
+import com.seoulmilk.emp.domain.value.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LoginResponse(
         @Schema(description = "사용자 정보")
-        UserInfo userInfo,
-
-        @Schema(description = "액세스 토큰")
-        String accessToken
-
+        UserInfo userInfo
 ) {
     public record UserInfo(
             @Schema(description = "사원번호", example = "12341234")
             String employeeId,
 
             @Schema(description = "사용자 이름", example = "홍길동")
-            String name
+            String name,
+
+            @Schema(description = "사용자 권한", example = "ROLE_USER")
+            Role role
     ) {
         public static UserInfo from(Emp employee) {
-            return new UserInfo(employee.getEmployeeId(), employee.getName());
+            return new UserInfo(employee.getEmployeeId(), employee.getName(), employee.getRole());
         }
     }
 
-    public static LoginResponse of(Emp employee, String accessToken) {
-        return new LoginResponse(UserInfo.from(employee), accessToken);
+    public static LoginResponse of(Emp employee) {
+        return new LoginResponse(UserInfo.from(employee));
     }
 }
