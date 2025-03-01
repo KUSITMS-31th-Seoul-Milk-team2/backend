8a5aeef0934c76b52efaa355934a0c90d16ba8dd
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-01 21:33:51 +0900

    feat: DummyData를 추가한다.

--- src/main/java/com/seoulmilk/core/application/EmpInitializationService.java
+++ src/main/java/com/seoulmilk/core/application/EmpInitializationService.java
@@ -18,13 +18,27 @@ public class EmpInitializationService {
 
     @PostConstruct
     public void init() {
-        empRepository.save(Emp.create(
-                "홍길동",
-                "12341234",
-                "test@test.com",
-                Role.ADMIN,
-                "010-1234-1234",
-                HashedPassword.of(passwordHashingService.hash(Password.from("12341234")).getValue())
-        ));
+
+        for (int i = 0; i < 5; i++) {
+            empRepository.save(Emp.create(
+                    "관리자" + i,
+                    "12341234" + i,
+                    "test" + i + "@test.com",
+                    Role.ADMIN,
+                    "010-1234-1234",
+                    HashedPassword.of(passwordHashingService.hash(Password.from("12341234")).getValue())
+            ));
+        }
+
+        for (int i = 0; i < 10; i++) {
+            empRepository.save(Emp.create(
+                    "사원" + i,
+                    "43214321" + i,
+                    "test" + i + "@test.com",
+                    Role.EMPLOYEE,
+                    "010-1234-1234",
+                    HashedPassword.of(passwordHashingService.hash(Password.from("12341234")).getValue())
+            ));
+        }
     }
 }
