78d32b3d049460d8aec47c4b8bd50577f24d2a10
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-01 22:30:03 +0900

    feat: LoginRequest 에 swagger 를 추가한다.

--- src/main/java/com/seoulmilk/auth/presentation/dto/request/LoginRequest.java
+++ src/main/java/com/seoulmilk/auth/presentation/dto/request/LoginRequest.java
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
         @NotNull(message = "사번은 필수 입력 항목입니다.")
-        @Schema(description = "사번", example = "12341234")
+        @Schema(description = "사번", example = "123412341")
         String employeeId,
 
         @NotNull(message = "비밀번호는 필수 입력 항목입니다.")
-        @Schema(description = "비밀번호", example = "password")
+        @Schema(description = "비밀번호", example = "12341234")
         String password
 ) {
 }
