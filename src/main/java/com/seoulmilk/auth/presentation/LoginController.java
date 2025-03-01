6713006e17654ee3436deb907f5b4049367ed79b
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-01 20:34:45 +0900

    feat: AccessToken을 쿠키로 넘겨준다.

--- src/main/java/com/seoulmilk/auth/presentation/LoginController.java
+++ src/main/java/com/seoulmilk/auth/presentation/LoginController.java
@@ -1,12 +1,18 @@
 package com.seoulmilk.auth.presentation;
 
 import com.seoulmilk.auth.application.LoginService;
+import com.seoulmilk.auth.application.TokenRequestFactory;
+import com.seoulmilk.auth.application.TokenService;
+import com.seoulmilk.auth.infrastructure.jwt.JwtProperties;
 import com.seoulmilk.auth.presentation.dto.request.LoginRequest;
 import com.seoulmilk.auth.presentation.dto.response.LoginResponse;
 import com.seoulmilk.auth.presentation.swagger.LoginSwagger;
 import com.seoulmilk.core.presentation.RestResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,11 +25,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController implements LoginSwagger {
 
     private final LoginService loginService;
+    private final TokenService tokenService;
+    private final JwtProperties jwtProperties;
 
     @Override
     @PostMapping("/login")
     public ResponseEntity<RestResponse<LoginResponse>> login(@Valid @RequestBody LoginRequest request) {
         LoginResponse loginResponse = loginService.login(request);
-        return ResponseEntity.ok(new RestResponse<>(loginResponse));
+        String accessToken = tokenService.provideAccessToken(TokenRequestFactory.create(request.employeeId()));
+
+        ResponseCookie cookie = ResponseCookie.from("accessToken", accessToken)
+                .maxAge(jwtProperties.getAccess().getExpiration())
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new RestResponse<>(loginResponse));
     }
 }
