471ae3f4a8c5a4ccc30eb0787e31c4ac4fe6c919
chyun7114 <chyun7114@naver.com>
2025-03-03 22:08:14 +0900

    chore: 오타 수정

--- src/main/java/com/seoulmilk/receipt/exception/ReceiptErrorCode.java
+++ src/main/java/com/seoulmilk/receipt/exception/ReceiptErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ReceiptErrorCode implements BaseErrorCode<DomainException> {
     ERROR_TO_CONNECT_CODEF_SERVER(HttpStatus.INTERNAL_SERVER_ERROR, "CODEF 서버와의 연결이 이뤄지지 않았습니다"),
-    ADDITIONAL_ATHENTICATION_ERROR(HttpStatus.NON_AUTHORITATIVE_INFORMATION, "추가인증 정보를 전달하지 못했습니다."),
+    ADDITIONAL_AUTHENTICATION_ERROR(HttpStatus.NON_AUTHORITATIVE_INFORMATION, "추가인증 정보를 전달하지 못했습니다."),
     ERROR_TO_GET_DATA(HttpStatus.INTERNAL_SERVER_ERROR, "응답 정보를 받는 것을 실패하였습니다."),
     INVALID_FORMAT_ERROR(HttpStatus.BAD_REQUEST, "잘못된 형식으로 요청값이 전달되었습니다.");
 
