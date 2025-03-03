cc639e7a6bbb4fe6e1f2919e48fb77e41c22aab0
chyun7114 <chyun7114@naver.com>
2025-03-03 20:56:14 +0900

    refactor: Factory클래스로 객체 생성 로직 분리 및 DIP 구현

--- src/main/java/com/seoulmilk/receipt/application/TaxReceiptValidationServiceImpl.java
+++ src/main/java/com/seoulmilk/receipt/application/TaxReceiptValidationServiceImpl.java
@@ -1,13 +1,10 @@
 package com.seoulmilk.receipt.application;
 
+import com.seoulmilk.receipt.infrastructure.EasyCodefFactory;
 import com.seoulmilk.receipt.infrastructure.EasyCodefProvider;
-import com.seoulmilk.receipt.infrastructure.webclient.TaxReceiptWebClientUtil;
 import com.seoulmilk.receipt.presentation.dto.request.TaxReceiptValidationRequest;
 import io.codef.api.EasyCodef;
-import io.codef.api.EasyCodefBuilder;
-import io.codef.api.constants.CodefClientType;
 import io.codef.api.dto.EasyCodefRequest;
-import io.codef.api.dto.EasyCodefRequestBuilder;
 import io.codef.api.dto.EasyCodefResponse;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -21,8 +18,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @Log4j2
 public class TaxReceiptValidationServiceImpl implements TaxReceiptValidationService {
-    private final TaxReceiptWebClientUtil taxReceiptWebClientUtil;
     private final EasyCodefProvider easyCodefProvider;
+    private final EasyCodefFactory easyCodefFactory;
 
     private EasyCodef easyCodef;
 
@@ -36,8 +33,7 @@ public class TaxReceiptValidationServiceImpl implements TaxReceiptValidationServ
         List<EasyCodefRequest> easyCodefRequests = new LinkedList<>();
 
         for(TaxReceiptValidationRequest request : requests) {
-            EasyCodefRequest easyCodefRequest = requestBuilder(request);
-            easyCodefRequests.add(easyCodefRequest);
+            easyCodefRequests.add(easyCodefFactory.createTaxReciptReQuest(request));
         }
 
         EasyCodefResponse response = easyCodef.requestMultipleProduct(easyCodefRequests);
@@ -46,29 +42,9 @@ public class TaxReceiptValidationServiceImpl implements TaxReceiptValidationServ
 
     @Override
     public List<EasyCodefResponse> requestMultipleTaxReceiptValidation (String transactionId) {
-        List<EasyCodefResponse> easyCodefResponses = easyCodef.requestMultipleSimpleAuthCertification(
-                transactionId
-        );
+        List<EasyCodefResponse> easyCodefResponses =
+                easyCodef.requestMultipleSimpleAuthCertification(transactionId);
 
         return easyCodefResponses;
-    }
-
-    private EasyCodefRequest requestBuilder(TaxReceiptValidationRequest request){
-        return EasyCodefRequestBuilder.builder()
-                .path("/v1/kr/public/nt/third-party/tax-invoice-issue")
-                .requestBody("organization", request.organization())
-                .requestBody("loginType", request.loginType())
-                .requestBody("id", request.id())
-                .requestBody("loginTypeLevel", request.loginTypeLevel())
-                .requestBody("userName", request.userName())
-                .requestBody("phoneNo", request.phoneNo())
-                .requestBody("identity", request.identity())
-                .requestBody("supplierRegNumber", request.supplierRegNumber())
-                .requestBody("contractorRegNumber", request.contractorRegNumber())
-                .requestBody("approvalNo", request.approvalNo())
-                .requestBody("reportingDate", request.reportingDate())
-                .requestBody("supplyValue", request.supplyValue())
-                .requestBody("telecom", request.telecom())
-                .build();
     }
 }
