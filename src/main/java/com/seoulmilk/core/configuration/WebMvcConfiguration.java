c012074109cbe58a672cddd70f7836bcfa2b836d
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-03 18:35:50 +0900

    chore: WebMvcConfiguration의 값을 환경변수로 관리한다.

--- src/main/java/com/seoulmilk/core/configuration/WebMvcConfiguration.java
+++ src/main/java/com/seoulmilk/core/configuration/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package com.seoulmilk.core.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+    @Value("${cors.allowed.paths}")
+    private String allowedPath;
+
+    @Value("${cors.allowed.origins}")
+    private String[] allowedOrigins;
+
+    @Value("${cors.allowed.methods}")
+    private String[] allowedMethods;
+
+    @Value("${cors.allowed.max-age}")
+    private long maxAge;
+
+    @Value("${cors.allowed.credentials}")
+    private boolean allowedCredentials;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://34.47.109.128")
-                .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .maxAge(3000);
+        System.out.println("allowedPath = " + allowedPath);
+        registry.addMapping(allowedPath)
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods(allowedMethods)
+                .allowCredentials(allowedCredentials)
+                .maxAge(maxAge);
     }
 }
