9f80c4b3365619b585a4eeb570e332d50c75b05b
chyun7114 <chyun7114@naver.com>
2025-03-04 11:54:48 +0900

    refactor: EasyCodef 관련 추상 클래스 분리 완료

---
+++ src/main/java/com/seoulmilk/receipt/application/TaxReceiptValidationProvider.java
@@ -0,0 +1,12 @@
+package com.seoulmilk.receipt.application;
+
+import com.seoulmilk.receipt.presentation.dto.request.TaxReceiptValidationRequest;
+import com.seoulmilk.receipt.presentation.dto.response.AdditionalAuthResponse;
+import com.seoulmilk.receipt.presentation.dto.response.TaxReceiptValidationResponse;
+
+import java.util.List;
+
+public interface TaxReceiptValidationProvider {
+    AdditionalAuthResponse requestAdditionalAuthentication(List<TaxReceiptValidationRequest> requests);
+    List<TaxReceiptValidationResponse> retrieveValidatedTaxReceipts  (String transactionId);
+}
