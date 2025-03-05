0e21ec5cbc707e3f8a38a03a9f6b6a9b5ad9ccd2
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-06 01:09:42 +0900

    feat: EmpErrorCode와 MailErrorCode를 생성한다.

--- src/main/java/com/seoulmilk/emp/exception/EmpErrorCode.java
+++ src/main/java/com/seoulmilk/emp/exception/EmpErrorCode.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum EmpErrorCode implements BaseErrorCode<DomainException> {
     FAILED_TO_SAVE_EMPLOYEE(HttpStatus.INTERNAL_SERVER_ERROR, "사원 정보를 저장하는데 실패하였습니다."),
-    NOT_EXIST_EMPLOYEE(HttpStatus.NOT_FOUND, "사원이 존재하지 않습니다.");
+    NOT_EXIST_EMPLOYEE(HttpStatus.NOT_FOUND, "사원이 존재하지 않습니다."),
+    WRONG_PASSWORD_ERROR(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    SAME_PASSWORD_ERROR(HttpStatus.BAD_REQUEST, "기존 비밀번호와 새 비밀번호가 동일합니다.");
 
     private final HttpStatus httpStatus;
 
