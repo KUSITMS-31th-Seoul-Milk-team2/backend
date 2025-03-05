fb0d5729639fad392134955427e61163dd35c89f
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-05 15:50:54 +0900

    feat: 파일 변환시 발생하는 에러를 정의한다.

--- src/main/java/com/seoulmilk/invoice/dto/response/OcrResponse.java
+++ src/main/java/com/seoulmilk/invoice/dto/response/OcrResponse.java
@@ -55,7 +55,5 @@ public record OcrResponse(
                 double height
         ) {
         }
-
-
     }
 }
--- src/main/java/com/seoulmilk/invoice/exception/InvoiceErrorCode.java
+++ src/main/java/com/seoulmilk/invoice/exception/InvoiceErrorCode.java
@@ -15,7 +15,8 @@ public enum InvoiceErrorCode implements BaseErrorCode<DomainException> {
     NOT_SUPPORTED_FILE(HttpStatus.BAD_REQUEST, "지원하지 않는 파일 형식입니다. (JPEG, PNG, PDF만 허용)"),
     INVALID_SUPPLIER_NUMBER_FORMAT(HttpStatus.BAD_REQUEST, "사업자등록번호 형식이 올바르지 않습니다."),
     INVALID_OCR_RESPONSE(HttpStatus.INTERNAL_SERVER_ERROR, "OCR 결과가 올바르지 않습니다."),
-    MISSING_REQUIRED_FIELD(HttpStatus.BAD_REQUEST, "필수 필드가 누락되었습니다."),;
+    MISSING_REQUIRED_FIELD(HttpStatus.BAD_REQUEST, "필수 필드가 누락되었습니다."),
+    FAILED_TO_PROCESS_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "세금 영수증 파일 처리 중 에러가 발생했습니다."),;
 
     private final HttpStatus httpStatus;
 
