6830750ea73d73dddac76c0f0969e15d4d082d5e
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-06 00:55:36 +0900

    feat: SendEmailRequest 와 팩토리를 생성한다.

---
+++ src/main/java/com/seoulmilk/core/util/factory/SendEmailRequestFactory.java
@@ -0,0 +1,12 @@
+package com.seoulmilk.core.util.factory;
+
+import com.seoulmilk.core.util.dto.SendEmailRequest;
+
+public class SendEmailRequestFactory {
+    private static final String TITLE = "서울우유 비밀번호 변경 안내";
+
+    public static SendEmailRequest create(String receiver, String newPassword) {
+        String content = "비밀번호가 변경되었습니다. 변경된 비밀번호는 " + newPassword + " 입니다.";
+        return new SendEmailRequest(receiver, TITLE, content);
+    }
+}
