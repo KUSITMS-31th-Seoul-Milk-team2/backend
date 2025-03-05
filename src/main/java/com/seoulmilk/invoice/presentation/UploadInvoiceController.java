2cd2c68e4c073f943c2fa8a4cd44df594e9b944e
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-05 15:59:52 +0900

    feat: 대량 파일 업로드를 비동기로 처리한다.

--- src/main/java/com/seoulmilk/invoice/presentation/UploadInvoiceController.java
+++ src/main/java/com/seoulmilk/invoice/presentation/UploadInvoiceController.java
@@ -1,17 +1,21 @@
 package com.seoulmilk.invoice.presentation;
 
 import com.seoulmilk.core.presentation.RestResponse;
-import com.seoulmilk.invoice.application.OpenFeignService;
+import com.seoulmilk.invoice.application.WebClientOcrService;
 import com.seoulmilk.invoice.dto.response.OcrResponse;
 import com.seoulmilk.invoice.presentation.swagger.UploadInvoiceSwagger;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,13 +23,20 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/v1/invoice")
 public class UploadInvoiceController implements UploadInvoiceSwagger {
 
-    public final OpenFeignService openFeignService;
+    public final WebClientOcrService webClientOcrService;
 
-    @PostMapping
-    public ResponseEntity<RestResponse<OcrResponse>> upload(
-            @RequestParam("file") MultipartFile file
-    ) {
-        OcrResponse ocrResponse =  openFeignService.processImg(file);
-        return ResponseEntity.ok(new RestResponse<>(ocrResponse));
+    @Override
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<RestResponse<Flux<OcrResponse>>> upload(@RequestPart("files") List<MultipartFile> files) {
+
+        for (MultipartFile file : files) {
+            webClientOcrService.processFile(file)
+                    .subscribe(
+                            response -> log.info("OCR 결과: {}", response),
+                            error -> log.error("OCR 처리 중 오류 발생", error)
+                    );
+        }
+        // TODO: 파일 OCR 처리 후 국세청 검증 결과를 반환해야함.
+        return ResponseEntity.ok(new RestResponse<>(Flux.empty()));
     }
 }
