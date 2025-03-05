f25423db48e373a124916ea31f9acb26c76b8075
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-06 01:22:49 +0900

    feat: LoginResponse 스펙을 수정한다.

--- src/main/java/com/seoulmilk/auth/presentation/dto/response/LoginResponse.java
+++ src/main/java/com/seoulmilk/auth/presentation/dto/response/LoginResponse.java
@@ -15,11 +15,17 @@ public record LoginResponse(
             @Schema(description = "사용자 이름", example = "홍길동")
             String name,
 
+            @Schema(description = "전화번호", example = "010-1234-1234")
+            String phoneNumber,
+
+            @Schema(description = "이메일", example = "test@test.com")
+            String email,
+
             @Schema(description = "사용자 권한", example = "ADMIN")
             Role role
     ) {
         public static UserInfo from(Emp employee) {
-            return new UserInfo(employee.getEmployeeId(), employee.getName(), employee.getRole());
+            return new UserInfo(employee.getEmployeeId(), employee.getName(), employee.getPhoneNumber(), employee.getEmail(), employee.getRole());
         }
     }
 
