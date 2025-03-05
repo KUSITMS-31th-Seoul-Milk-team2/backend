f310583a2d1a025d1030b6b238bcaf37d4baeba4
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-06 01:08:22 +0900

    feat: JwtAuthenticationFilter를 생성한다.

---
+++ src/main/java/com/seoulmilk/core/infrastructure/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.seoulmilk.core.infrastructure.filter;
+
+import com.seoulmilk.auth.infrastructure.jwt.JwtTokenProvider;
+import com.seoulmilk.core.application.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailsService userDetailsService;
+    //    private static final String BEARER = "Bearer ";
+//    private static final String AUTHORIZATION = "Authorization";
+    private static final String ACCESS_TOKEN_COOKIE = "accessToken";
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return path.startsWith("/v1/auth");
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = resolveToken(request);
+        if (token != null) {
+            Authentication auth = createAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        Cookie accessTokenCookie = WebUtils.getCookie(request, ACCESS_TOKEN_COOKIE);
+        return (accessTokenCookie != null) ? accessTokenCookie.getValue() : null;
+    }
+
+    private Authentication createAuthentication(String token) {
+        Long empPk = jwtTokenProvider.getEmpId(token);
+        log.info("JwtAuthenticationFilter 속 token : {}", token);
+        log.info("JwtAuthenticationFilter 속 empId : {}", empPk);
+        UserDetails userDetails = userDetailsService.loadUserById(empPk);
+        log.info("JwtAuthenticationFilter 속 userDetails : {}", userDetails);
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+}
