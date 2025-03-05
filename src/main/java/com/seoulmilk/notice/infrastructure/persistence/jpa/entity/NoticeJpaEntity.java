c2ba0b112528239a272f642f49cbaeed38ec453f
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-06 01:32:31 +0900

    feat: 공지 엔티티와 매퍼를 생성한다.

---
+++ src/main/java/com/seoulmilk/notice/infrastructure/persistence/jpa/entity/NoticeJpaEntity.java
@@ -0,0 +1,41 @@
+package com.seoulmilk.notice.infrastructure.persistence.jpa.entity;
+
+import com.seoulmilk.core.infrastructure.jpa.entity.BaseLongIdEntity;
+import com.seoulmilk.notice.domain.entity.Notice;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Optional;
+
+@Entity
+@Getter
+@SuperBuilder
+@Table(name = "NOTICE")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NoticeJpaEntity extends BaseLongIdEntity {
+    private String employeeId;
+
+    private String title;
+
+    private String content;
+
+    private String fileUrl;
+
+    public static NoticeJpaEntity toJpaEntity(Notice notice) {
+        return NoticeJpaEntity.builder()
+                .id(notice.getId())
+                .employeeId(notice.getEmployeeId())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .fileUrl(notice.getFileUrl())
+                .createdAt(notice.getCreatedAt())
+                .updatedAt(notice.getUpdatedAt())
+                .deleted(Optional.ofNullable(notice.getDeleted()).orElse(false))
+                .build();
+    }
+
+}
