3df30ee1dc5d3bb0b8759b72fc321935ec566817
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-06 16:10:52 +0900

    feat: 에러 코드를 추가한다.

--- src/main/java/com/seoulmilk/core/exception/error/GlobalErrorCode.java
+++ src/main/java/com/seoulmilk/core/exception/error/GlobalErrorCode.java
@@ -8,14 +8,17 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GlobalErrorCode implements BaseErrorCode<RuntimeException> {
 
-  ALREADY_PROCESS_STARTED(HttpStatus.BAD_REQUEST, "이미 처리중인 요청입니다."),
-  INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 내부 오류입니다.");
-  private final HttpStatus httpStatus;
+    ALREADY_PROCESS_STARTED(HttpStatus.BAD_REQUEST, "이미 처리중인 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 내부 오류입니다."),
+    INVALID_FILE(HttpStatus.BAD_REQUEST, "유효하지 않은 파일입니다."),
+    FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "써드파티 저장소에 파일 업로드 중 오류가 발생했습니다.");
 
-  private final String message;
+    private final HttpStatus httpStatus;
 
-  @Override
-  public RuntimeException toException() {
-    return new RuntimeException(message);
-  }
+    private final String message;
+
+    @Override
+    public RuntimeException toException() {
+        return new RuntimeException(message);
+    }
 }
