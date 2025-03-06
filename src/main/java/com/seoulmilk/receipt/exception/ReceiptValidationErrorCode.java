9cd350f8dcfccde397661f10bfbab5a3fe7866bf
chyun7114 <chyun7114@naver.com>
2025-03-06 23:05:43 +0900

    feat:세금계산서 transactionId 이용해서 받아오기

--- src/main/java/com/seoulmilk/receipt/exception/ReceiptValidationErrorCode.java
+++ src/main/java/com/seoulmilk/receipt/exception/ReceiptValidationErrorCode.java
@@ -12,7 +12,8 @@ public enum ReceiptValidationErrorCode implements BaseErrorCode<DomainException>
     ERROR_TO_CONNECT_CODEF_SERVER(HttpStatus.INTERNAL_SERVER_ERROR, "CODEF 서버와의 연결이 이뤄지지 않았습니다"),
     ADDITIONAL_AUTHENTICATION_ERROR(HttpStatus.NON_AUTHORITATIVE_INFORMATION, "추가인증 정보를 전달하지 못했습니다."),
     ERROR_TO_GET_DATA(HttpStatus.INTERNAL_SERVER_ERROR, "응답 정보를 받는 것을 실패하였습니다."),
-    INVALID_FORMAT_ERROR(HttpStatus.BAD_REQUEST, "잘못된 형식으로 요청값이 전달되었습니다.");
+    INVALID_FORMAT_ERROR(HttpStatus.BAD_REQUEST, "잘못된 형식으로 요청값이 전달되었습니다."),
+    NOT_EXIST_TXID(HttpStatus.NOT_FOUND, "계산서 정보를 찾는데 실패하였습니다.");
 
     private final HttpStatus httpStatus;
 
