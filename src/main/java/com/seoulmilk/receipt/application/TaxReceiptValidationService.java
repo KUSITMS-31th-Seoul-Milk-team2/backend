5ad734252cf44ff7b65feccb7020982dc2801a0b
chyun7114 <chyun7114@naver.com>
2025-03-06 22:12:03 +0900

    refactor: 중복 코드를 제거한다

--- src/main/java/com/seoulmilk/receipt/application/TaxReceiptValidationService.java
+++ src/main/java/com/seoulmilk/receipt/application/TaxReceiptValidationService.java
@@ -1,29 +1,21 @@
 package com.seoulmilk.receipt.application;
 
-import com.seoulmilk.auth.infrastructure.jwt.JwtTokenProvider;
 import com.seoulmilk.emp.domain.entity.Emp;
 import com.seoulmilk.emp.domain.repository.EmpRepository;
 import com.seoulmilk.emp.exception.EmpErrorCode;
-import com.seoulmilk.emp.infrastructure.persistence.repository.EmpJpaRepository;
 import com.seoulmilk.receipt.dto.request.OcrValidationRequest;
 import com.seoulmilk.receipt.dto.request.TaxReceiptValidationRequest;
-import com.seoulmilk.receipt.exception.ReceiptErrorCode;
 import com.seoulmilk.receipt.infrastructure.factory.TaxReceiptValidationRequestFactory;
 import com.seoulmilk.receipt.presentation.dto.response.AdditionalAuthResponse;
 import com.seoulmilk.receipt.presentation.dto.response.TaxReceiptValidationResponse;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwt;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -36,30 +28,40 @@ public class TaxReceiptValidationService {
 
     @KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.group-id}")
     public void listen(OcrValidationRequest ocrValidationRequest) {
-        // 이것도 캐싱하고 싶은데 방법이....
-        Emp emp = empRepository.findByid(ocrValidationRequest.empPk())
-                .orElseThrow(() -> EmpErrorCode.NOT_EXIST_EMPLOYEE.toException());;
-        log.info("[TaxReceiptValidationService] emp 전화번호 - {}", emp.getPhoneNumber());
-        String cacheKey = "uuid:" + ocrValidationRequest.empPk();
-        String uuid = getUUID(cacheKey);
+        Emp emp = getEmployee(ocrValidationRequest.empPk());
 
-        TaxReceiptValidationRequest taxReceiptValidationRequest = TaxReceiptValidationRequestFactory.create(
-                emp, ocrValidationRequest, uuid
-        );
+        String uuid = getUUID("uuid:" + ocrValidationRequest.empPk());
+        TaxReceiptValidationRequest taxReceiptValidationRequest = createTaxReceiptValidationRequest(emp, ocrValidationRequest, uuid);
 
 
-        log.info("[TaxReceiptValidationService] 요청 데이터 - {}", taxReceiptValidationRequest);
+        AdditionalAuthResponse additionalAuthResponse = requestAdditionalAuthentication(List.of(taxReceiptValidationRequest));
 
-        AdditionalAuthResponse additionalAuthResponse
-                = requestAdditionalAuthentication(List.of(taxReceiptValidationRequest));
+        handleTransactionId(ocrValidationRequest.empPk(), additionalAuthResponse.jti());
+    }
 
-        log.info("[TaxReceiptValidationService] 받은 transactionId - {}"
-                    , additionalAuthResponse.jti());
-        String transactionCacheKey = "transactionId:" + ocrValidationRequest.empPk();
-        String transactionId = (String) redisTemplate.opsForValue().get(transactionCacheKey);
-        if(transactionId == null) {
-            redisTemplate.opsForValue().set(cacheKey, additionalAuthResponse.jti());
+    private Emp getEmployee(Long empPk) {
+        return empRepository.findByid(empPk)
+                .orElseThrow(() -> EmpErrorCode.NOT_EXIST_EMPLOYEE.toException());
+    }
+
+    private TaxReceiptValidationRequest createTaxReceiptValidationRequest(Emp emp, OcrValidationRequest ocrValidationRequest, String uuid) {
+        return TaxReceiptValidationRequestFactory.create(emp, ocrValidationRequest, uuid);
+    }
+
+    private void handleTransactionId(Long empPk, String transactionId) {
+        String transactionCacheKey = "transactionId:" + empPk;
+        if (redisTemplate.opsForValue().get(transactionCacheKey) == null) {
+            redisTemplate.opsForValue().set(transactionCacheKey, transactionId, Duration.ofMinutes(5));
         }
+    }
+
+    private String getUUID(String cacheKey) {
+        String uuid = (String) redisTemplate.opsForValue().get(cacheKey);
+        if (uuid == null) {
+            uuid = UUID.randomUUID().toString();
+            redisTemplate.opsForValue().set(cacheKey, uuid, Duration.ofMinutes(3));
+        }
+        return uuid;
     }
 
     public AdditionalAuthResponse requestAdditionalAuthentication(List<TaxReceiptValidationRequest> requests) {
@@ -68,16 +70,5 @@ public class TaxReceiptValidationService {
 
     public List<TaxReceiptValidationResponse> retrieveValidatedTaxReceipts(String transactionId) {
         return taxReceiptValidationProvider.retrieveValidatedTaxReceipts(transactionId);
-    }
-
-    private String getUUID(String cacheKey){
-        String uuid = (String) redisTemplate.opsForValue().get(cacheKey);
-
-        if (uuid == null) {
-            uuid = UUID.randomUUID().toString();
-            redisTemplate.opsForValue().set(cacheKey, uuid, Duration.ofSeconds(120));
-        }
-
-        return uuid;
     }
 }
--- src/main/java/com/seoulmilk/receipt/dto/request/TaxReceiptValidationRequest.java
+++ src/main/java/com/seoulmilk/receipt/dto/request/TaxReceiptValidationRequest.java
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.NotNull;
 
 public record TaxReceiptValidationRequest(
         @NotNull(message = "기관명은 필수 입력 항목입니다.")
-        @Schema(description = "기관코드", example = "0004(고정값)")
+        @Schema(description = "기관코드", example = "0004")
         String organization,
 
         @NotNull(message = "로그인 구분은 필수 입력 항목입니다.")
-        @Schema(description = "로그인 구분", example = "0 -> 인증서 로그인, 5 -> 간편인증")
+        @Schema(description = "로그인 구분", example = "5")
         String loginType,
 
         @NotNull(message = "사용자를 구분하기 위한 유일값을 세팅해주세요")
@@ -17,44 +17,43 @@ public record TaxReceiptValidationRequest(
         String id,
 
         @NotNull(message = "간편인증시 로그인 구분은 필수 인증 값입니다.")
-        @Schema(description = "간편인증 로그인 구분", example = "1:카카오톡, 2:페이코, 3:삼성패스, 4:KB모바일, 5:통신사(PASS), " +
-                "6:네이버, 7:신한인증서, 8: toss, 9: 뱅크샐러드")
+        @Schema(description = "간편인증 로그인 구분", example = "1")
         String loginTypeLevel,
 
         @NotNull(message = "사용자 이름은 필수 입력 항목입니다.")
-        @Schema(description = "사용자 이름", example = "홍길동")
+        @Schema(description = "사용자 이름", example = "윤창현")
         String userName,
 
         @NotNull(message = "전화번호는 필수 입력 항목입니다.")
-        @Schema(description = "전화번호", example = "010-1234-5678")
+        @Schema(description = "전화번호", example = "개인 전화번호(숫자만)")
         String phoneNo,
 
         @NotNull(message = "사용자 주민번호는 필수 입력 항목입니다.")
-        @Schema(description = "사용자 주민번호 앞자리", example = "YYYYMMDD")
+        @Schema(description = "사용자 주민번호 앞자리", example = "개인정보(YYYYMMDD)")
         String identity,
 
         @NotNull(message = "공급자 등록 번호는 필수 입력 항목입니다.")
-        @Schema(description = "공급자 등록 번호", example = "10자리 숫자 (1234567890)")
+        @Schema(description = "공급자 등록 번호", example = "3062870320")
         String supplierRegNumber,
 
         @NotNull(message = "공급 받는자 등록번호는 필수 입력 항목입니다.")
-        @Schema(description = "공급받는자 등록번호", example = "10자리 숫자 (1234567890)")
+        @Schema(description = "공급받는자 등록번호", example = "3088509085")
         String contractorRegNumber,
 
         @NotNull(message = "승인번호는 필수 입력 항목입니다.")
-        @Schema(description = "승인번호", example = "숫자만 입력하셔야 합니다")
+        @Schema(description = "승인번호", example = "202406304100000578475123")
         String approvalNo,
 
         @NotNull(message = "작성일자는 필수 입력 항목입니다.")
-        @Schema(description = "작성일자", example = "YYYYMMDD")
+        @Schema(description = "작성일자", example = "20240630")
         String reportingDate,
 
         @NotNull(message = "공급가액은 필수 입력 항목입니다.")
-        @Schema(description = "공급가액", example = "4560000")
+        @Schema(description = "공급가액", example = "23930493")
         String supplyValue,
 
         @NotNull(message = "간편인증시 통신사는 필수 입력 항목입니다.")
-        @Schema(description = "통신사", example = "“0\":SKT(SKT알뜰폰), “1”:KT(KT알뜰폰), “2\":LG U+(LG U+알뜰폰)")
+        @Schema(description = "통신사", example = "0")
         String telecom
 ) {}
 
