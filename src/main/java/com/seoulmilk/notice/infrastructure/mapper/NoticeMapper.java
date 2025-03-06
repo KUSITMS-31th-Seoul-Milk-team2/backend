14b6b359be479ccf03ac3dd2a4bd7bbd3248b9e5
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-06 16:11:11 +0900

    feat: 공지 엔티티를 생성한다.

--- src/main/java/com/seoulmilk/notice/infrastructure/mapper/NoticeMapper.java
+++ src/main/java/com/seoulmilk/notice/infrastructure/mapper/NoticeMapper.java
@@ -2,7 +2,9 @@ package com.seoulmilk.notice.infrastructure.mapper;
 
 import com.seoulmilk.notice.domain.entity.Notice;
 import com.seoulmilk.notice.infrastructure.persistence.jpa.entity.NoticeJpaEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NoticeMapper {
     public Notice toDomainEntity(NoticeJpaEntity notice) {
         return Notice.toDomainEntity(notice);
