aceb21b32ae5a47733ea331c00af891b3947795e
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-06 16:10:23 +0900

    feat: FileStorageService와 구현체를 생성한다.

---
+++ src/main/java/com/seoulmilk/core/infrastructure/FileStorageServiceImpl.java
@@ -0,0 +1,61 @@
+package com.seoulmilk.core.infrastructure;
+
+import com.seoulmilk.core.application.FileStorageService;
+import com.seoulmilk.core.exception.error.GlobalErrorCode;
+import io.minio.*;
+import io.minio.http.Method;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class FileStorageServiceImpl implements FileStorageService {
+
+    private final MinioClient minioClient;
+
+    @Value("${minio.bucket}")
+    private String bucketName;
+
+    @PostConstruct
+    void initBucket() throws Exception {
+        if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+        }
+    }
+
+    @Override
+    public String uploadFile(MultipartFile file) throws Exception {
+        if (file == null || file.isEmpty()) {
+            throw GlobalErrorCode.INVALID_FILE.toException();
+        }
+
+        String objectName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(objectName)
+                        .stream(file.getInputStream(), file.getSize(), -1)
+                        .contentType(file.getContentType())
+                        .build()
+        );
+
+        return getPresignedUrl(objectName);
+    }
+
+    private String getPresignedUrl(String objectName) throws Exception {
+        return minioClient.getPresignedObjectUrl(
+                GetPresignedObjectUrlArgs.builder()
+                        .method(Method.GET)
+                        .bucket(bucketName)
+                        .object(objectName)
+                        .expiry(7, TimeUnit.DAYS) // TODO: 1년간 유효한 URL로 변경
+                        .build()
+        );
+    }
+}
