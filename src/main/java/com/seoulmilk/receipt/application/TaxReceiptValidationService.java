47d28250a32ee08bda3e3aef866f5af55f5f3481
chyun7114 <chyun7114@naver.com>
2025-03-06 14:23:03 +0900

    fix : kafka 파티션 개수 조절

--- src/main/java/com/seoulmilk/receipt/application/TaxReceiptValidationService.java
+++ src/main/java/com/seoulmilk/receipt/application/TaxReceiptValidationService.java
@@ -1,17 +1,26 @@
 package com.seoulmilk.receipt.application;
 
+import com.seoulmilk.receipt.dto.request.OcrValidationRequest;
 import com.seoulmilk.receipt.presentation.dto.request.TaxReceiptValidationRequest;
 import com.seoulmilk.receipt.presentation.dto.response.AdditionalAuthResponse;
 import com.seoulmilk.receipt.presentation.dto.response.TaxReceiptValidationResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class TaxReceiptValidationService {
     private final TaxReceiptValidationProvider taxReceiptValidationProvider;
+
+    @KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.group-id}")
+    public void listen(OcrValidationRequest ocrValidationRequest) {
+        log.info("[TaxReceiptValidationService] ocr 결과 받은 데이터 - {}", ocrValidationRequest.toString());
+    }
 
     public AdditionalAuthResponse requestAdditionalAuthentication(List<TaxReceiptValidationRequest> requests) {
         return taxReceiptValidationProvider.requestAdditionalAuthentication(requests);
