34908a1633f9d2851bc0363652c1d3d2faf7ac59
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-08 00:42:40 +0900

    feat: 비동기 configuration을 추가한다.

---
+++ src/main/java/com/seoulmilk/batch/infra/configuration/AsyncConfiguration.java
@@ -0,0 +1,24 @@
+package com.seoulmilk.batch.infra.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfiguration {
+
+    @Bean(name = "ocrTaskExecutor")
+    public Executor ocrTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("OCR-Thread-");
+        executor.initialize();
+        return executor;
+    }
+}
