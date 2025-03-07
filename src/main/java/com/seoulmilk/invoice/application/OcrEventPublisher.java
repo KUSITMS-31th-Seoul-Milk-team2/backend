1ba50a81e677f5eeea5bfb264af8b45b7db0f4b1
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-08 00:43:59 +0900

    feat: 카프카 직렬화 및 역직렬화를 수정한다.

--- src/main/java/com/seoulmilk/invoice/application/OcrEventPublisher.java
+++ src/main/java/com/seoulmilk/invoice/application/OcrEventPublisher.java
@@ -1,5 +1,9 @@
 package com.seoulmilk.invoice.application;
 
+import com.seoulmilk.receipt.dto.request.OcrValidationRequest;
+
+import java.util.List;
+
 public interface OcrEventPublisher {
-    void publish(Object event);
+    void publish(List<OcrValidationRequest> event);
 }
