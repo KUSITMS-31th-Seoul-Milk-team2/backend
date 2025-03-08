126ab34fe4d1d28cf8cca438d8c38aa14c2fef0c
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-08 23:51:35 +0900

    feat: 역직렬화기를 커스텀한다.

---
+++ src/main/java/com/seoulmilk/core/configuration/kafka/deserializer/OcrValidationRequestListDeserializer.java
@@ -0,0 +1,57 @@
+package com.seoulmilk.core.configuration.kafka.deserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.seoulmilk.receipt.dto.request.OcrValidationRequest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OcrValidationRequestListDeserializer extends StdDeserializer<List<OcrValidationRequest>> {
+
+    public OcrValidationRequestListDeserializer() {
+        this(null);
+    }
+
+    public OcrValidationRequestListDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public List<OcrValidationRequest> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        List<OcrValidationRequest> result = new ArrayList<>();
+
+        if (p.currentToken() == JsonToken.START_ARRAY) {
+            p.nextToken();
+            
+            if (p.currentToken() == JsonToken.START_ARRAY) {
+                while (p.nextToken() != JsonToken.END_ARRAY) {
+                    result.add(deserializeSingleRequest(p, ctxt));
+                }
+                p.nextToken();
+            }
+
+            else {
+                while (p.currentToken() != JsonToken.END_ARRAY) {
+                    result.add(deserializeSingleRequest(p, ctxt));
+                    p.nextToken();
+                }
+            }
+        }
+
+        else if (p.currentToken() == JsonToken.START_OBJECT) {
+            result.add(deserializeSingleRequest(p, ctxt));
+        }
+
+        return result;
+    }
+
+    private OcrValidationRequest deserializeSingleRequest(JsonParser p, DeserializationContext ctxt) throws IOException {
+        JavaType type = ctxt.getTypeFactory().constructType(OcrValidationRequest.class);
+        return ctxt.readValue(p, type);
+    }
+}
