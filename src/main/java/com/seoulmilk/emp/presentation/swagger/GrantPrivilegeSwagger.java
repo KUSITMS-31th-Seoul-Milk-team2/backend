d68a987f2b16192b9ac2e0d25e2b85fcea0a0c99
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-08 23:42:40 +0900

    feat: GrantPrivilegeController와 스웨거를 생성한다.

--- src/main/java/com/seoulmilk/emp/presentation/swagger/GrantPrivilegeSwagger.java
+++ src/main/java/com/seoulmilk/emp/presentation/swagger/GrantPrivilegeSwagger.java
@@ -4,27 +4,28 @@ import com.seoulmilk.core.configuration.swagger.ApiErrorCode;
 import com.seoulmilk.core.exception.error.GlobalErrorCode;
 import com.seoulmilk.core.infrastructure.security.CustomUserDetails;
 import com.seoulmilk.core.presentation.RestResponse;
-import com.seoulmilk.emp.dto.request.UpdatePasswordRequest;
+import com.seoulmilk.emp.dto.request.GrantPrivilegeRequest;
+import com.seoulmilk.emp.dto.response.GrantPrivilegeResponse;
+import com.seoulmilk.emp.exception.AdminErrorCode;
 import com.seoulmilk.emp.exception.EmpErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Tag(name = "MyPage", description = "마이페이지")
-public interface UpdatePasswordSwagger {
+@Tag(name = "Admin", description = "관리자")
+public interface GrantPrivilegeSwagger {
     @Operation(
-            summary = "비밀번호 변경 API",
-            description = "비밀번호를 변경합니다.",
-            operationId = "/v1/emp/update-password"
+            summary = "권한 부여 API",
+            description = "사원에게 권한을 부여합니다.",
+            operationId = "/v1/emp/grant-privilege"
     )
-    @ApiErrorCode({GlobalErrorCode.class, EmpErrorCode.class})
-    ResponseEntity<RestResponse<String>> updatePassword(
+    @ApiErrorCode({GlobalErrorCode.class, EmpErrorCode.class, AdminErrorCode.class})
+    ResponseEntity<RestResponse<GrantPrivilegeResponse>> grantPrivilege(
             @Parameter(hidden = true)
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
-            @RequestBody UpdatePasswordRequest updatePasswordRequest
+            @RequestBody GrantPrivilegeRequest grantPrivilegeRequest
     );
 }
