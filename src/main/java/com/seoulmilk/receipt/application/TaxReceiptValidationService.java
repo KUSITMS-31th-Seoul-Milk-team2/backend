174c6c36c3fb78faa3800b239e27ab8950a013ed
chyun7114 <chyun7114@naver.com>
2025-03-10 14:42:52 +0900

    fix: 임포트 오류 수정

--- src/main/java/com/seoulmilk/receipt/application/TaxReceiptValidationService.java
+++ src/main/java/com/seoulmilk/receipt/application/TaxReceiptValidationService.java
@@ -25,6 +25,7 @@ import org.springframework.stereotype.Service;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
