318ec5198d3548d714d451b2a7c97c5ad07cec13
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-11 00:35:00 +0900

    feat: AT를 body로도 전달한다.

--- src/main/java/com/seoulmilk/auth/presentation/dto/response/LoginResponse.java
+++ src/main/java/com/seoulmilk/auth/presentation/dto/response/LoginResponse.java
@@ -5,6 +5,9 @@ import com.seoulmilk.emp.domain.value.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LoginResponse(
+        @Schema(description = "Access Token")
+        String accessToken,
+
         @Schema(description = "사용자 정보")
         UserInfo userInfo
 ) {
@@ -29,7 +32,7 @@ public record LoginResponse(
         }
     }
 
-    public static LoginResponse of(Emp employee) {
-        return new LoginResponse(UserInfo.from(employee));
+    public static LoginResponse of(String accessToken, Emp employee) {
+        return new LoginResponse(accessToken, UserInfo.from(employee));
     }
 }
