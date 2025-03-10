f9b30767f39900ac084b8a2ede776141f6e82f5d
chyun7114 <chyun7114@naver.com>
2025-03-10 12:39:40 +0900

    feat: 세금 계산서 업데이트 쿼리 작성

---
+++ src/main/java/com/seoulmilk/receipt/presentation/swagger/TaxReceiptUpdateSwagger.java
@@ -0,0 +1,24 @@
+package com.seoulmilk.receipt.presentation.swagger;
+
+import com.seoulmilk.core.presentation.RestResponse;
+import com.seoulmilk.receipt.domain.entity.ValidReceipt;
+import com.seoulmilk.receipt.dto.request.UpdateValidReceiptRequest;
+import com.seoulmilk.receipt.dto.request.ValidResponseSearchRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Tag(name = "CRUD", description = "세금계산서 관련 crud")
+public interface TaxReceiptUpdateSwagger {
+    @Operation(
+            summary = "세금계산서 정보 수정 API",
+            description = "검증이 완료된 세금계산서의 정보를 수정합니다.",
+            operationId = "/v1/receipt/update"
+    )
+    ResponseEntity<RestResponse<Boolean>> updateValidReceipt(
+            @RequestBody UpdateValidReceiptRequest updateValidReceiptRequest
+    );
+}
