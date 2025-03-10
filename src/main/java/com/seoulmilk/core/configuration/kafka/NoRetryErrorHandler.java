5e437e6d8971644bbb474fdcc8a86c97c7966b49
chyun7114 <chyun7114@naver.com>
2025-03-10 14:18:24 +0900

    feat: 카프카 에러 로깅 추가

--- src/main/java/com/seoulmilk/core/configuration/kafka/NoRetryErrorHandler.java
+++ src/main/java/com/seoulmilk/core/configuration/kafka/NoRetryErrorHandler.java
@@ -1,14 +1,17 @@
 package com.seoulmilk.core.configuration.kafka;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.listener.KafkaListenerErrorHandler;
 import org.springframework.kafka.listener.ListenerExecutionFailedException;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 @Component("noRetryErrorHandler")
+@Log4j2
 public class NoRetryErrorHandler implements KafkaListenerErrorHandler {
     @Override
     public Object handleError(Message<?> message, ListenerExecutionFailedException exception) {
+        log.error("Kafka 리스너 에러 발생: {}, 메시지: {}", exception.getMessage(), message, exception);
         return null;
     }
 }
