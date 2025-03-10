0df309e13e0eedd41801a89c2aad60c8245f490a
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-10 15:20:41 +0900

    hotfix: 공지 길이 제한을 변경한다.

--- src/main/java/com/seoulmilk/notice/infrastructure/persistence/jpa/entity/NoticeJpaEntity.java
+++ src/main/java/com/seoulmilk/notice/infrastructure/persistence/jpa/entity/NoticeJpaEntity.java
@@ -25,7 +25,7 @@ public class NoticeJpaEntity extends BaseLongIdEntity {
 
     private String title;
 
-    @Lob
+    @Column(length = 1024)
     private String content;
 
     @Column(length = 512)
