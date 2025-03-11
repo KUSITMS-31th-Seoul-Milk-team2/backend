a910c87a49edb767c6dff613fbc91fb4c731b4ea
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-11 19:01:20 +0900

    feat: EmpRepository에 쿼리를 추가한다.

--- src/main/java/com/seoulmilk/emp/infrastructure/persistence/repository/EmpJpaRepository.java
+++ src/main/java/com/seoulmilk/emp/infrastructure/persistence/repository/EmpJpaRepository.java
@@ -1,7 +1,9 @@
 package com.seoulmilk.emp.infrastructure.persistence.repository;
 
-import com.seoulmilk.emp.domain.entity.Emp;
+import com.seoulmilk.emp.dto.response.FilteredEmpResponse;
 import com.seoulmilk.emp.infrastructure.persistence.jpa.entity.EmpJpaEntity;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -31,4 +33,29 @@ public interface EmpJpaRepository extends JpaRepository<EmpJpaEntity, Long> {
                 where e.id = :id
             """)
     void grantPrivilege(Long id);
+
+    List<EmpJpaEntity> findAllByIdIn(List<Long> ids);
+
+    @Query("SELECT e FROM EmpJpaEntity e ORDER BY e.id DESC")
+    List<EmpJpaEntity> findAllOrderByIdDesc(Pageable pageable);
+
+    @NotNull
+    @Query("""
+            SELECT new com.seoulmilk.emp.dto.response.FilteredEmpResponse(
+                e.id,
+                e.name,
+                e.employeeId,
+                e.role
+            )
+            FROM EmpJpaEntity e
+            """)
+    List<FilteredEmpResponse> findAllWithNeededInfo();
+
+    @Modifying
+    @Query("""
+                update EmpJpaEntity e
+                set e.hometax = :hometaxName
+                where e.id = :empPk
+            """)
+    void updateHometaxInfo(Long empPk, String hometaxName);
 }
