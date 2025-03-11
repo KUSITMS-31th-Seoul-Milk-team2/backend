37a1ebe1389d5250af9f169b80ac1d7bfde50b90
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-11 19:49:05 +0900

    refactor: 공지 content 타입을 CLOB으로 변경한다.

--- src/main/java/com/seoulmilk/emp/dto/request/DeleteEmpsRequest.java
+++ src/main/java/com/seoulmilk/emp/dto/request/DeleteEmpsRequest.java
@@ -1,8 +1,11 @@
 package com.seoulmilk.emp.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record DeleteEmpsRequest(
+        @Schema(description = "삭제 할 사원 ID 리스트", example = "[1, 2, 3]")
         List<Long> ids
 ) {
 }
