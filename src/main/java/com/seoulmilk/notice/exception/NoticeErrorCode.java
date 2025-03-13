6509d089e6859ad961fb126ddb1d73d631ed48f7
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-13 12:52:44 +0900

    feat: NoticeErrorCode를 추가한다.

--- src/main/java/com/seoulmilk/notice/exception/NoticeErrorCode.java
+++ src/main/java/com/seoulmilk/notice/exception/NoticeErrorCode.java
@@ -11,7 +11,8 @@ import org.springframework.http.HttpStatus;
 public enum NoticeErrorCode implements BaseErrorCode<DomainException> {
     NOT_EXISTS_NOTICE(HttpStatus.BAD_REQUEST, "존재하지 않는 공지사항입니다."),
     NOT_AN_AUTHOR(HttpStatus.BAD_REQUEST, "자신이 쓴 공지만 수정 또는 삭제가 가능합니다."),
-    INVALID_SEARCH_KEYWORD(HttpStatus.BAD_REQUEST, "검색조건이 존재하지 않습니다.");
+    INVALID_SEARCH_KEYWORD(HttpStatus.BAD_REQUEST, "검색조건이 존재하지 않습니다."),
+    POST_NOTICE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "공지사항 등록에 실패하였습니다."),;
 
 
     private final HttpStatus httpStatus;
