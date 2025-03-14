be1359e2a9f2337276313514de9eea5895ce65f5
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-14 14:53:30 +0900

    chore: 캐시 Configuration 을 설정한다.

--- src/main/java/com/seoulmilk/BackendApplication.java
+++ src/main/java/com/seoulmilk/BackendApplication.java
@@ -3,6 +3,7 @@ package com.seoulmilk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ConfigurationPropertiesScan
 @EnableFeignClients
 @EnableScheduling
+@EnableCaching
 public class BackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/seoulmilk/core/configuration/cache/CacheConfiguration.java
@@ -0,0 +1,49 @@
+package com.seoulmilk.core.configuration.cache;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.seoulmilk.notice.dto.response.PageNoticeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+@RequiredArgsConstructor
+public class CacheConfiguration {
+    @Bean
+    RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new Jackson2JsonRedisSerializer<>(objectMapper, PageNoticeResponse.class)
+                        )).entryTtl(Duration.ofMinutes(10));
+    }
+
+    @Bean
+    public CacheManager customCacheManager(RedisConnectionFactory redisConnectionFactory, RedisCacheConfiguration redisCacheConfiguration) {
+
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(Caffeine.newBuilder()
+                .maximumSize(100)
+                .expireAfterWrite(Duration.ofMinutes(10)));
+
+        RedisCacheManager redisCacheManager = RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+
+        return new CustomCacheManager(caffeineCacheManager, redisCacheManager);
+    }
+}
