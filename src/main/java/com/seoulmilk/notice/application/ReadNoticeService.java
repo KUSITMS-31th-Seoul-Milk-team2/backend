0f01f604879804b7cb4243a252b0840e93482bb2
HoyeongJeon <hoyeong0302@gmail.com>
2025-03-31 02:11:29 +0900

    feat: 캐시 TTL을 변경한다.

--- src/main/java/com/seoulmilk/notice/application/ReadNoticeService.java
+++ src/main/java/com/seoulmilk/notice/application/ReadNoticeService.java
@@ -21,7 +21,6 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
