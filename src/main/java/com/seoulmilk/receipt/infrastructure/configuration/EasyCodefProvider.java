0b926bcd05ffd69239efe02ab8e325c46e9b55db
HoyeongJeon <hoyeong0302@gmail.com>
2025-04-07 00:42:10 +0900

    chore: EasyCodef 만료로 인한 코드 주석처리

--- src/main/java/com/seoulmilk/receipt/infrastructure/configuration/EasyCodefProvider.java
+++ src/main/java/com/seoulmilk/receipt/infrastructure/configuration/EasyCodefProvider.java
@@ -1,23 +1,19 @@
 package com.seoulmilk.receipt.infrastructure.configuration;
 
-import com.seoulmilk.receipt.infrastructure.properties.EasyCodefProperties;
-import io.codef.api.EasyCodef;
-import io.codef.api.EasyCodefBuilder;
-import io.codef.api.constants.CodefClientType;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
 public class EasyCodefProvider {
-    private final EasyCodef easyCodef;
-
-    public EasyCodefProvider(EasyCodefProperties easyCodefProperties) {
-        this.easyCodef = EasyCodefBuilder.builder()
-                .clientType(CodefClientType.DEMO)
-                .clientId(easyCodefProperties.getClientId())
-                .clientSecret(easyCodefProperties.getClientSecret())
-                .publicKey(easyCodefProperties.getPublicKey())
-                .build();
-    }
+//    private final EasyCodef easyCodef;
+//
+//    public EasyCodefProvider(EasyCodefProperties easyCodefProperties) {
+//        this.easyCodef = EasyCodefBuilder.builder()
+//                .clientType(CodefClientType.DEMO)
+//                .clientId(easyCodefProperties.getClientId())
+//                .clientSecret(easyCodefProperties.getClientSecret())
+//                .publicKey(easyCodefProperties.getPublicKey())
+//                .build();
+//    }
 }
--- src/main/java/com/seoulmilk/receipt/infrastructure/service/EasyCodefValidationService.java
+++ src/main/java/com/seoulmilk/receipt/infrastructure/service/EasyCodefValidationService.java
@@ -8,9 +8,6 @@ import com.seoulmilk.receipt.infrastructure.factory.EasyCodefRequestFactory;
 import com.seoulmilk.receipt.dto.request.TaxReceiptValidationRequest;
 import com.seoulmilk.receipt.presentation.dto.response.AdditionalAuthResponse;
 import com.seoulmilk.receipt.presentation.dto.response.TaxReceiptValidationResponse;
-import io.codef.api.EasyCodef;
-import io.codef.api.dto.EasyCodefRequest;
-import io.codef.api.dto.EasyCodefResponse;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -24,74 +21,74 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 @Log4j2
-public class EasyCodefValidationService implements TaxReceiptValidationProvider {
+public class EasyCodefValidationService {
     private final EasyCodefProvider easyCodefProvider;
     private final EasyCodefRequestFactory easyCodefRequestFactory;
     private final ObjectMapper objectMapper;
 
-    private EasyCodef easyCodef;
-
-    @PostConstruct
-    private void setEasyCodef(){
-        easyCodef = easyCodefProvider.getEasyCodef();
-    }
-
-    @Override
-    public AdditionalAuthResponse requestAdditionalAuthentication(List<TaxReceiptValidationRequest> requests) {
-        List<EasyCodefRequest> easyCodefRequests = new LinkedList<>();
-
-        for(TaxReceiptValidationRequest request : requests) {
-            easyCodefRequests.add(easyCodefRequestFactory.createTaxReceiptRequest(request));
-        }
-
-        EasyCodefResponse response = null;
-        try{
-            response = easyCodef.requestMultipleProduct(easyCodefRequests);
-        } catch (Exception e){
-            throw ReceiptValidationErrorCode.ERROR_TO_CONNECT_CODEF_SERVER.toException();
-        }
-
-        if(isSuccess(response.code())){
-            HashMap<String, Object> responseMap = objectMapper.convertValue(response, HashMap.class);
-            return objectMapper.convertValue(responseMap.get("data"), AdditionalAuthResponse.class);
-        }else{
-            throw ReceiptValidationErrorCode.ERROR_TO_GET_DATA.toException();
-        }
-    }
-
-    @Override
-    public List<TaxReceiptValidationResponse> retrieveValidatedTaxReceipts  (String transactionId) {
-        List<EasyCodefResponse> easyCodefResponses = new ArrayList<>();
-
-        try {
-            List<EasyCodefResponse> tempResponses = easyCodef.requestMultipleSimpleAuthCertification(transactionId);
-            if (tempResponses != null) {
-                easyCodefResponses = tempResponses;
-            }
-        } catch (NullPointerException e) {
-            System.err.println("NullPointerException 발생: requestMultipleSimpleAuthCertification 결과가 null이거나 접근 중 문제 발생");
-        }
-
-        List<TaxReceiptValidationResponse> validationResponses = new LinkedList<>();
-
-        for(EasyCodefResponse easyCodefResponse : easyCodefResponses){
-            if(isSuccess(easyCodefResponse.code())){
-                HashMap<String, Object> responseMap = objectMapper.convertValue(easyCodefResponse, HashMap.class);
-                validationResponses.add(objectMapper.convertValue(responseMap.get("data"), TaxReceiptValidationResponse.class));
-            }else{
-                throw ReceiptValidationErrorCode.INVALID_FORMAT_ERROR.toException();
-            }
-        }
-
-        return validationResponses;
-    }
-
-    private boolean isSuccess(String code){
-        if(code.equals("CF-00000"))
-            return true;
-        else if(code.equals("CF-03002"))
-            return true;
-        else
-            return false;
-    }
+//    private EasyCodef easyCodef;
+//
+//    @PostConstruct
+//    private void setEasyCodef(){
+//        easyCodef = easyCodefProvider.getEasyCodef();
+//    }
+//
+//    @Override
+//    public AdditionalAuthResponse requestAdditionalAuthentication(List<TaxReceiptValidationRequest> requests) {
+//        List<EasyCodefRequest> easyCodefRequests = new LinkedList<>();
+//
+//        for(TaxReceiptValidationRequest request : requests) {
+//            easyCodefRequests.add(easyCodefRequestFactory.createTaxReceiptRequest(request));
+//        }
+//
+//        EasyCodefResponse response = null;
+//        try{
+//            response = easyCodef.requestMultipleProduct(easyCodefRequests);
+//        } catch (Exception e){
+//            throw ReceiptValidationErrorCode.ERROR_TO_CONNECT_CODEF_SERVER.toException();
+//        }
+//
+//        if(isSuccess(response.code())){
+//            HashMap<String, Object> responseMap = objectMapper.convertValue(response, HashMap.class);
+//            return objectMapper.convertValue(responseMap.get("data"), AdditionalAuthResponse.class);
+//        }else{
+//            throw ReceiptValidationErrorCode.ERROR_TO_GET_DATA.toException();
+//        }
+//    }
+//
+//    @Override
+//    public List<TaxReceiptValidationResponse> retrieveValidatedTaxReceipts  (String transactionId) {
+//        List<EasyCodefResponse> easyCodefResponses = new ArrayList<>();
+//
+//        try {
+//            List<EasyCodefResponse> tempResponses = easyCodef.requestMultipleSimpleAuthCertification(transactionId);
+//            if (tempResponses != null) {
+//                easyCodefResponses = tempResponses;
+//            }
+//        } catch (NullPointerException e) {
+//            System.err.println("NullPointerException 발생: requestMultipleSimpleAuthCertification 결과가 null이거나 접근 중 문제 발생");
+//        }
+//
+//        List<TaxReceiptValidationResponse> validationResponses = new LinkedList<>();
+//
+//        for(EasyCodefResponse easyCodefResponse : easyCodefResponses){
+//            if(isSuccess(easyCodefResponse.code())){
+//                HashMap<String, Object> responseMap = objectMapper.convertValue(easyCodefResponse, HashMap.class);
+//                validationResponses.add(objectMapper.convertValue(responseMap.get("data"), TaxReceiptValidationResponse.class));
+//            }else{
+//                throw ReceiptValidationErrorCode.INVALID_FORMAT_ERROR.toException();
+//            }
+//        }
+//
+//        return validationResponses;
+//    }
+//
+//    private boolean isSuccess(String code){
+//        if(code.equals("CF-00000"))
+//            return true;
+//        else if(code.equals("CF-03002"))
+//            return true;
+//        else
+//            return false;
+//    }
 }
